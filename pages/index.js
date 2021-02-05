d02835218fa9ae00c73f143d7ff33119f1919bca
Joshua <legioncommander555@gmail.com>
2021-02-05 23:52:01 +0700

    stuff

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({albums}) {
         <meta name="aye" content='aye' />
       </Head>
       <div className="grid grid-cols-4 gap-8 p-10">
-        {albums && albums.albums.map(album => 
+        {albums.albums.map(album => 
           <h1 key={album._id}>{album.name}</h1>
         )}
       </div>
@@ -21,10 +21,10 @@ export default function Home({albums}) {
 export async function getServerSideProps(context) {
   let res;
   if (process.env.NODE_ENV === 'development') {
-    res = await fetch(`http://localhost:3333/api/users/?email=legion@gmail.com`)
-  } else {
-    res = await fetch(`https://nextjs-sandbox-server.herokuapp.com/api/users/?email=legion@gmail.com`)
-  }
+      res = await fetch(`http://localhost:3333/api/users/?email=legion@gmail.com`)
+    } else {
+      res = await fetch(`https://nextjs-sandbox-server.herokuapp.com/api/users/?email=legion@gmail.com`)
+    }
   const albums = await res.json()
 
   if (!albums) {
