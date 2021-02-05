7111214d2b31136f170772bc5a78a09c87e82b4a
Joshua <legioncommander555@gmail.com>
2021-02-05 23:37:23 +0700

    mess

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({albums}) {
         <meta name="aye" content='aye' />
       </Head>
       <div className="grid grid-cols-4 gap-8 p-10">
-        {albums.albums.map(album => 
+        {albums && albums.albums.map(album => 
           <h1 key={album._id}>{album.name}</h1>
         )}
       </div>
@@ -18,9 +18,7 @@ export default function Home({albums}) {
   )
 }
 
-
-
-export const getStaticProps = async () => {
+export async function getServerSideProps(context) {
   let res;
   if (process.env.NODE_ENV === 'development') {
     res = await fetch(`http://localhost:3333/api/users/?email=legion@gmail.com`)
@@ -29,9 +27,32 @@ export const getStaticProps = async () => {
   }
   const albums = await res.json()
 
+  if (!albums) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
     props: {
       albums
     }
   }
 }
+
+
+// export const getStaticProps = async () => {
+//   let res;
+//   if (process.env.NODE_ENV === 'development') {
+//     res = await fetch(`http://localhost:3333/api/users/?email=legion@gmail.com`)
+//   } else {
+//     res = await fetch(`https://nextjs-sandbox-server.herokuapp.com/api/users/?email=legion@gmail.com`)
+//   }
+//   const albums = await res.json()
+
+//   return {
+//     props: {
+//       albums
+//     }
+//   }
+// }
