73154001fe04c32d29811214c30e7ec3e22e6070
Joshua <legioncommander555@gmail.com>
2021-02-10 17:46:52 +0700

    fix env key

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
       domains: ['firebasestorage.googleapis.com'],
     },
+    env: {
+      serverAPI: 'https://nodejs-image-server-304317.ew.r.appspot.com',
+    },
   }
